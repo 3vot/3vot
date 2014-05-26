8c7ad1cb0d281812c6e4597b2b2263e82627a261
Roberto Rodriguez <roberto@Robertos-MacBook-Air-2.local>
2014-05-25 20:44:43 -0600

    update 3vot bug with ;

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ window._3vot = {
   height: "",
   user_name: "",
   unique_query_string: "unique_domain=" + window.location.protocol + window.location.hostname
-}
+};
 
 module.exports= init;
 
 function init(package, options){
-  if(!options) options = {}
+  if(!options) options = {};
   _3vot.initOptions = options;
   _3vot.package = package;
 
@@ -23,8 +23,8 @@ function init(package, options){
   _3vot.endpoint= options.endpoint || "http://backend.3vot.com/v1";
   _3vot.frontpoint= options.frontpoint || "http://daulau2emlz5i.cloudfront.net";
   _3vot.domain= options.domain || "//3vot.com";
-  _3vot.path= _3vot.domain+"/"+_3vot.user_name+"/"+package.name
-  _3vot.host= _3vot.frontpoint.split("://")[1]
+  _3vot.path= _3vot.domain+"/"+_3vot.user_name+"/"+package.name;
+  _3vot.host= _3vot.frontpoint.split("://")[1];
   _3vot.el = document.getElementById('_3vot_' + package.name);
   _3vot.app = package.name;
   loadFile( fileToLoad() );
@@ -43,16 +43,16 @@ function fileToLoad(){
   var fileToLoad = "index";
   var width = document.documentElement.clientWidth;
   for(entry in entries){
-    var borders = entry.split("-")
+    var borders = entry.split("-");
     if(width > parseInt(borders[0]) && width <= parseInt(borders[1]) ){
       fileToLoad = entries[entry];
     }
   }
 
-  if(width <= 550) _3vot.device = "phone"
-  else if(width <= 1024) _3vot.device = "tablet"
-  else if(width > 1024) _3vot.device = "desktop"
-  else _3vot.device = "desktop"
+  if(width <= 550) _3vot.device = "phone";
+  else if(width <= 1024) _3vot.device = "tablet";
+  else if(width > 1024) _3vot.device = "desktop";
+  else _3vot.device = "desktop";
 
 
   if(!fileToLoad) return console.error("Could not determine a file to load from package.json threevot.screens. 3VOT will load index.js please fix package.json of your app.");
@@ -60,7 +60,7 @@ function fileToLoad(){
 }
 
 function loadFile(fileToCall){
-  if(!fileToCall) return false 
+  if(!fileToCall) return false;
   var script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = _3vot.path + '/' + fileToCall + '.js?' + _3vot.unique_query_string;    
