bbd9b4df4a832772a606b60782486219c93ac41d
Roberto Rodriguez <roberto@rodriguezartavia.com>
2014-06-28 11:26:19 -0600

    add *.

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function loadFile(fileToCall){
   if(!fileToCall) return false;
   var script = document.createElement('script');
   script.type = 'text/javascript';
-  script.src =  fileToCall + '.js?' + _3vot.unique_query_string;    
+  script.src =  "*/" + fileToCall + '.js?' + _3vot.unique_query_string;    
 
   document.getElementsByTagName('head')[0].appendChild(script);
 
